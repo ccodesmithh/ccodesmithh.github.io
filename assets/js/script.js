b948344e1a6a36db770c16dd626880c21e5b5b2a
user <user@192.168.1.6>
2025-08-15 19:15:38 +0700

    Hero Animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,71 @@
-anime({
+anime.timeline()
+.add({
+    targets: '.circle',
+    keyframes: [
+        { opacity: 0, duration: 150 },
+        { opacity: 1, duration: 150 },
+        { opacity: 0, duration: 150 },
+        { opacity: 1, duration: 150 },
+        { opacity: 0, duration: 150 },
+        { opacity: 1, duration: 150 }
+    ],
+    easing: 'easeInOutSine'
+})
+.add({
+    targets: '.circle',
+    opacity: [0, 1],
+    duration: 1000,
+    easing: 'easeInOutQuad'
+})
+.add({
+  targets: '.nav-links',
+  opacity: [0, 1],
+  duration: 1000,
+  easing: 'easeInOutQuad'
+})
+
+.add({
   targets: '.animated-text',
   translateX: [40, 0],
   opacity: [0, 1],
   easing: 'easeOutQuad',
-  duration: 800
-});
+}, "-=700")
 
-anime({
+.add({
   targets: '.animated-text-name',
   translateX: [40, 0],
   opacity: [0, 1],
   easing: 'easeOutQuad',
-  duration: 800,
-  delay: 300 // muncul setelah .animated-text
-});
+}, "-=700")
 
-anime({
+.add({
   targets: '.animated-text-role',
   translateX: [40, 0],
   opacity: [0, 1],
   easing: 'easeOutQuad',
-  duration: 800,
-  delay: 600 // muncul terakhir
-});
+}, "-=700")
+
+.add({
+  targets: '.content-social',
+  opacity: [0, 1],
+  easing: 'easeOutQuad',
+}, "-=800")
+
+.add({
+  targets: '.buttons',
+  opacity: [0, 1],
+  easing: 'easeOutQuad',
+}, "-=800")
+
+.add({
+  targets: '.content',
+  opacity: [0, 1],
+  easing: 'easeOutQuad',
+}, "-=900")
+
+.add({
+  targets: '.photo',
+  translateY: [-400, -470],
+  opacity: [0, 1],
+  easing: 'easeOutQuad',
+}, "-=700");
